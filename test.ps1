c01246aeee1b85e77e63d7ab5f839f2224ce6bb7
Alena Hrynkevich <alena@appveyor.com>
2019-08-05 12:11:04 -0700

    Removed quatation marks around ps commands

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
-"$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\CleanCluster.ps1"
-"$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1"
-"$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\CleanCluster.ps1"
+$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\CleanCluster.ps1
+$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1
+$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\ClusterSetup\CleanCluster.ps1
